2562b05f82b1d96a7d12a423b84fa1120d777c88
Marcin Potoczny <marcin.potoczny@protonmail.com>
2024-11-12 17:09:29 +0100

    Change database to postgresql and configure it to work with render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,9 +77,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
 }
 
